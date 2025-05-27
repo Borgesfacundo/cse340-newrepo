ca08b45bbde9c58a1f3730396dea4625464045ca
Borgesfacundo <bor22016@byui.edu>
2025-05-27 08:17:33 -0600

    w02 starting

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = 
+require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,10 +26,11 @@ app.set("layout", "./layouts/layout") //not at views root
  *************************/
 app.use(static)
 
+// Inventory Routes
+app.use("/inventory", inventoryRoute)
+
 //Index route
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
