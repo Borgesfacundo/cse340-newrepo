088a5bb98ca0ea598de3221f4ae92358c152428e
Borgesfacundo <borgesfacundo@gmail.com>
2025-05-29 15:35:10 -0600

    w03 Middleware added to all routes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,18 @@
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
+const utilities = require("../utilities/");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(invController.buildByClassificationId)
+);
 
 // Route to build a view for single cars
-router.get("/detail/:inv_id", invController.buildSingleCar);
+router.get(
+  "/detail/:inv_id",
+  utilities.handleErrors(invController.buildSingleCar)
+);
 
 module.exports = router;
