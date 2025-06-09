989f2ffa60d45de0157bc36cc234a00d75ad5d05
Borgesfacundo <borgesfacundo@gmail.com>
2025-06-09 15:39:59 -0600

    w05 addd link to open repairs from single vehicle view

--- utilities/index.js
+++ utilities/index.js
@@ -104,6 +104,8 @@ Util.buildSingleCar = async function (data) {
   grid += `<p><b>Description: </b> ${car.inv_description}</p>`;
   grid += `<p><b>Color: </b> ${car.inv_color}</p>`;
   grid += `<p><b>Miles: </b> ${car.inv_miles.toLocaleString()}</p>`;
+  //Add link to repair details
+  grid += `<a href="/inventory/repairs/${car.inv_id}" class="repair-link">View Repair History</a>`;
   grid += "</div>";
   grid += "</div>";
   return grid;
